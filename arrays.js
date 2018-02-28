522d1fba94deed4e149f407060342825ac5b600e
Francisco Gutierrez <fjgutierrez112@gmail.com>
2018-02-28 20:07:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-array = [element, ...array];
+return array = [element, ...array];
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  array.unshift(element);
+return array.unshift(element);
 }
 
 function addElementToEndOfArray(array, element) {
