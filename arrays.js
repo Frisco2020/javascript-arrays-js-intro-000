55c349edbd7c4a6f9535339e973353d2d692b4d1
Francisco Gutierrez <fjgutierrez112@gmail.com>
2018-02-28 19:41:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
 }
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+}
